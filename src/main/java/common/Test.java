bbfefe4f5f6f9a1787f3b1c723a9bbf84263ef23
zhangMr <775383634@qq.com>
2019-11-29 20:50:05 +0800

    local 1

---
+++ src/main/java/common/Test.java
@@ -0,0 +1,16 @@
+package common;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Test {
+    public static void main(String[] args) {
+        StringUtils.isEmpty(null);
+
+        StringUtils.isNotEmpty(null);
+
+        StringUtils.isBlank(null);
+
+        StringUtils.isNotBlank(null);
+
+    }
+}
